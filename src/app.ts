1ee24dd1e53411c3f9cb392d40fd36f1ece99e96
cameng <adisaputracameng@gmail.com>
2020-10-15 13:47:39 +0800

    Update app.ts and contact.router.ts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express, {Application, Request, Response} from 'express';
+import express, { Application, Request, Response } from 'express';
+
 const app: Application = express();
 const port: number = 3000;
 
